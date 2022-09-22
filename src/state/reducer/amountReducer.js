972d0626a8dd62fb20d6840d058a5f426a429830
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-22 12:31:06 +0530

    Improved

--- src/state/reducer/amountReducer.js
+++ src/state/reducer/amountReducer.js
@@ -2,7 +2,11 @@ const reducer = (state = 0, action) => {
   if (action.type === "deposit") {
     return state + action.payload;
   } else if (action.type === "withdraw") {
-    return state - action.payload;
+    if (state === 0 || state < 0) {
+      alert("Balance can't be negative");
+    } else {
+      return state - action.payload;
+    }
   } else {
     return state;
   }
